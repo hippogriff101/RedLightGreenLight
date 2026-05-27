4bca391076780b7ce0611e6b5f035cd721de91a7
Freddie Yershon <143739931+hippogriff101@users.noreply.github.com>
2026-05-27 19:05:18 +0100

    Integrate sound alerts for light signals
    Added sound notifications for RED and GREEN messages.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from playsound import playsound
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
@@ -14,5 +15,7 @@ while True:
     
     if data.decode().strip() == "RED":
         print("Red light")
+        playsound("red-light.mp3")
     elif data.decode().strip() == "GREEN":
         print("Green Light!")
+        playsound("green-light.mp3")
